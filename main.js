39a0c97e9f959764d51daf062b8be5f4a3274853
Miguel Falcão <f4lcom@hotmail.com>
2024-02-23 17:03:27 -0300

    fix: limitacao de casas decimais

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function atualizatabela(){
 function atualizamediafinal() {
         const mediafinal = calculamediafinal()
 
-        document.getElementById('media-final-valor').innerHTML = mediafinal
+        document.getElementById('media-final-valor').innerHTML = mediafinal.toFixed(2)
         document.getElementById('media-final-resultado').innerHTML = mediafinal >= notaminima ? spanaprovado : spanreprovado
        
 }
